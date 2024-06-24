86aebd5b279bbb60aec79f998b1f30a4f433f88d
Sakin790 <sakinislam79@gmail.com>
2024-06-24 13:01:10 +0600

    refactor

--- 05_00P/Super.py
+++ 05_00P/Super.py
@@ -5,6 +5,7 @@ class Car:
     def full_name(self):
         return (f"{self.brand} {self.model}")
 
+
 class Electric(Car):
     def __init__(self, brand, model, battery_size):
        super().__init__(brand, model)
