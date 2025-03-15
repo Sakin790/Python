2297508e4695dd655afad86f6b916445280a7860
Sakin790 <sakinislam79@gmail.com>
2025-03-16 02:01:06 +0600

    isEmpty

--- Linked List/main.py
+++ Linked List/main.py
@@ -2,3 +2,11 @@ class Node:
     def __init__(self,item=None,next=None):
         self.item=item
         self.next=next
+
+class SSl:
+    def __init__(self,start=None):
+        self.start=start
+    def isEmpty(self):
+        if (self.start==None):
+            return "node is empty"
+        
