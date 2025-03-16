ba01b49278e7dc0892aa751b9b321be939ed2e4f
Sakin790 <sakinislam79@gmail.com>
2025-03-17 02:23:05 +0600

    i have basic problem

--- Linked List/main.py
+++ Linked List/main.py
@@ -1,19 +1,43 @@
-         
 class Node:
-    def __init__(self,item=None,next=None):
-        self.item=item
-        self.next=next
+    def __init__(self, item=None, next=None):
+        self.item = item
+        self.next = next
 
 class SSl:
-    def __init__(self,start=None):
-        self.start=start
+    def __init__(self, start=None):
+        self.start = start
+
     def isEmpty(self):
         return self.start is None
 
-    def insertAtFirst(self,data):
+    def insertAtFirst(self, data):
         newNode = Node(data)
         newNode.next = self.start
         self.start = newNode
-     
 
-myLinkedList=SSl(12)
+    def insertAtLast(self, data):
+        newNode = Node(data)
+        if self.isEmpty():
+            self.start = newNode
+        else:
+            temp = self.start
+            while temp.next is not None:
+                temp = temp.next
+            temp.next = newNode
+
+    def printList(self):
+        if self.isEmpty():
+            print("Node is empty")
+            return
+        temp = self.start
+        while temp is not None: 
+            print(temp.item, end=" -> ")
+            temp = temp.next
+        print("None")  
+
+
+myLinkedList = SSl()
+myLinkedList.insertAtFirst(10)
+myLinkedList.insertAtLast(20)
+myLinkedList.insertAtLast(30)
+myLinkedList.printList()
